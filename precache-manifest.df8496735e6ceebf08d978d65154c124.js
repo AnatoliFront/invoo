175e120c33264f9103d20df89f7412ed0af2919b
Anatoly Kupava <anatolykupava@MacBook-Pro-Anatoly.local>
2020-02-05 12:36:45 +0300

    first commit

---
+++ precache-manifest.df8496735e6ceebf08d978d65154c124.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46566ed91781c90ccb15c9689762f984",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94cd2f709f8c58c85c4f",
+    "url": "/static/css/2.aa7fcd32.chunk.css"
+  },
+  {
+    "revision": "5556b8bcbc342f8d79a1",
+    "url": "/static/css/main.c847d949.chunk.css"
+  },
+  {
+    "revision": "94cd2f709f8c58c85c4f",
+    "url": "/static/js/2.28bc7381.chunk.js"
+  },
+  {
+    "revision": "5556b8bcbc342f8d79a1",
+    "url": "/static/js/main.d44ea3d6.chunk.js"
+  },
+  {
+    "revision": "0526eb4604b4ecff3417",
+    "url": "/static/js/runtime~main.2e0096bc.js"
+  },
+  {
+    "revision": "eee1d7f3a026acdc5f42658316e40a78",
+    "url": "/static/media/apteka.eee1d7f3.jpg"
+  }
+]);
